6c85d76b3e532c5f9800ce686f0c90a7adeb6694
Varun Rajput <rvarun011@gmail.com>
2024-08-30 06:55:47 -0700

    init devenv

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, lib, config, ... }:
+
+{
+  packages = [
+    # A python dependency outside of poetry.
+    config.languages.python.package.pkgs.pjsua2
+  ];
+
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+      install = {
+        enable = true;
+        installRootPackage = false;
+        onlyInstallRootPackage = false;
+        compile = false;
+        quiet = false;
+        groups = [ ];
+        ignoredGroups = [ ];
+        onlyGroups = [ ];
+        extras = [ ];
+        allExtras = false;
+        verbosity = "no";
+      };
+      activate.enable = true;
+      package = pkgs.poetry;
+    };
+  };
+
+  env.PYTHONPATH = ".";
+}
